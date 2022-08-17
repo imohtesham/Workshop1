5120d9256eb523e3df133c73c8ced354269cdf29
imohtesham <ibraahimm92@gmail.com>
2022-08-17 12:40:43 +0530

    hi new updated

---
+++ Workshop1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Workshop1
+{
+     class TicTacToe
+
+    {
+        public void button1()
+        {
+            Console.WriteLine("Game tictactoe");
+        }
+        public void buttonclick(object sender, EventArgs a)
+        {
+
+        }
+        public void playAll(object sender,EventArgs a)
+        {
+
+        }
+        public void resetgame(object sender,EventArgs a)
+        {
+
+        }
+       static void Main (string[] args)
+        {
+
+        }
+        
+
+        
+    }
+    
+}
---
+++ Workshop1/Tictactoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop1
+{
+    class Tictactoa : TicTacToe
+    {
+        int x;
+        int y;
+        public void player1()
+        {
+
+        }
+        public void comp()
+        {
+
+        }
+
+    }
+    
+
+}
